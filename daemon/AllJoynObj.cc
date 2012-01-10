863c61fba893f0e7cdc39333e37163641c1c195b
Jack Profit <jprofit@quicinc.com>
2012-01-10 15:19:38 -0800

    ALLJOYN-886: RunAttach was incorrectly holding a pointer to an endpoint after giving up the lock

--- daemon/AllJoynObj.cc
+++ daemon/AllJoynObj.cc
@@ -1354,6 +1354,7 @@ qcc::ThreadReturn STDCALL AllJoynObj::JoinSessionThread::RunAttach()
                 status = ajObj.SendAttachSession(sessionPort, src, sessionHost, dest, b2bEpName.c_str(), nextControllerName.c_str(),
                                                  msg->GetSessionId(), busAddr, optsIn, replyCode, tempId, tempOpts, replyArgs[3]);
                 ajObj.AcquireLocks();
+                b2bEp = NULL;
 
                 /* If successful, add bi-directional session routes */
                 if ((status == ER_OK) && (replyCode == ALLJOYN_JOINSESSION_REPLY_SUCCESS)) {
