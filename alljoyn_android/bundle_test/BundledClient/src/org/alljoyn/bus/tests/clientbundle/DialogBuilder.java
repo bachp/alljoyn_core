6ffcb6034156f48fa78fb74e6128dfe52d32450c
Tony Wallace <awallace@quicinc.com>
2012-03-14 14:27:13 -0700

    add Bundle Tests

---
+++ alljoyn_android/bundle_test/BundledClient/src/org/alljoyn/bus/tests/clientbundle/DialogBuilder.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010-2012, Qualcomm Innovation Center, Inc.
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.alljoyn.bus.tests.clientbundle;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DialogBuilder {
+
+	private static final String TAG = "client.Dialogs";
+    
+	private TextView mFailReason;
+	
+    public Dialog createDebugDialog(final Activity activity, final String reason)
+    {
+    	
+    	Log.i(TAG, "createDebugDialog()");
+    	final Dialog dialog = new Dialog(activity);
+    	dialog.requestWindowFeature(dialog.getWindow().FEATURE_NO_TITLE);
+    	
+    	dialog.setContentView(R.layout.debug);
+    	
+    	mFailReason = (TextView) dialog.findViewById(R.id.failReason);
+    	mFailReason.setText(reason);
+    	
+    	Button cancel = (Button)dialog.findViewById(R.id.clientCancel);
+    	cancel.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View view) {
+				/*
+				 * Android likes to reuse dialogs for performance reasons. 
+				 * Tell the Android
+				 * application framework to forget about this dialog completely.
+				 */
+    			activity.removeDialog(Client.DIALOG_DEBUG_ID);
+    		}
+    	});
+    	
+    	return dialog;
+    }
+}
---
+++ alljoyn_android/bundle_test/BundledService/src/org/alljoyn/bus/tests/bundledservice/ServiceInterface.java
@@ -0,0 +1,25 @@
+package org.alljoyn.bus.tests.bundledservice;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.annotation.BusInterface;
+import org.alljoyn.bus.annotation.BusMethod;
+/*
+* The BusInterface annotation is used to tell the code that this interface is an AllJoyn interface.
+*
+* The 'name' value is used to specify by which name this interface will be known.  If the name is
+* not given the fully qualified name of the Java interface is be used.  In most instances its best
+* to assign an interface name since it helps promote code reuse.
+*/
+@BusInterface(name = "org.alljoyn.bus.tests.BundledService")
+public interface ServiceInterface {
+	   /*
+     * The BusMethod annotation signifies that this function should be used as part of the AllJoyn
+     * interface.  The runtime is smart enough to figure out what the input and output of the method
+     * is based on the input/output arguments of the Ping method.
+     *
+     * All methods that use the BusMethod annotation can throw a BusException and should indicate
+     * this fact.
+     */
+    @BusMethod
+    String Ping(String inStr) throws BusException;
+}
