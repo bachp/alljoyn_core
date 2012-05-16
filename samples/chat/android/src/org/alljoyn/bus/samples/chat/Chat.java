47a548b6845266d39c14a95ea18db2f45c3e7dbf
Eric Rongo <erongo@quicinc.com>
2012-05-16 12:56:20 -0700

    ALLJOYN-883 moved alljoyn calls out of UI thread

--- samples/chat/android/src/org/alljoyn/bus/samples/chat/Chat.java
+++ samples/chat/android/src/org/alljoyn/bus/samples/chat/Chat.java
@@ -16,24 +16,22 @@
 
 package org.alljoyn.bus.samples.chat;
 
-import java.lang.reflect.Array;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
-import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MenuInflater;
-import android.view.inputmethod.EditorInfo;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,7 +42,7 @@ import android.widget.Toast;
 
 public class Chat extends Activity {
 
-	private EditText editText;    
+    private EditText editText;
     private ArrayAdapter<String> listViewArrayAdapter;
     private ListView listView;
     private Menu menu;
@@ -52,6 +50,11 @@ public class Chat extends Activity {
     static final int DIALOG_CONNECT = 1;
     static final int DIALOG_ADVERTISE = 2;
     static final int DIALOG_JOIN_SESSION = 3;
+
+
+    private static final int INCOMINGMESSAGE = 1;
+    private static final int SESSIONCREATEFAILED = 2;
+    private static final int SENDCHATFAILED = 3;
 
     /** Called when activity's onCreate is called */
     private native int jniOnCreate(String packageName);
@@ -64,17 +67,32 @@ public class Chat extends Activity {
 
     /** Called when user enters a name to be "Advertised" " */
     private native boolean advertise(String wellKnownName);
-    
+
     /** Called when user wants to join a session */
     private native boolean joinSession(String sessionName);
 
     /** Handler used to post messages from C++ into UI thread */
+
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            listViewArrayAdapter.add((String)msg.obj);
+            switch (msg.what)
+            {
+            case INCOMINGMESSAGE:
+                listViewArrayAdapter.add((String)msg.obj);
+                break;
+            case SESSIONCREATEFAILED:
+                Toast.makeText(Chat.this, "jniOnCreate failed with  " + msg.arg1, Toast.LENGTH_LONG).show();
+                finish();
+                break;
+            case SENDCHATFAILED:
+                Log.v("AllJoynChat", String.format("sendChatMsg(%s) failed (%d)", msg.obj, msg.arg1));
+                break;
+            }
         }
     };
+
+    private BusHandler bus_handler;
 
     /* Prevent orientation changes */
     @Override
@@ -87,55 +105,52 @@ public class Chat extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.choice);
         // Ask what would you like to do Advertise a session or join one
         final RadioButton radio_create_seesion = (RadioButton) findViewById(R.id.radio_create_session);
         final RadioButton radio_join_session = (RadioButton) findViewById(R.id.radio_join_session);
         radio_create_seesion.setOnClickListener(radio_listener);
         radio_join_session.setOnClickListener(radio_listener);
-                
+
         setConnectedState(false);
 
-        // Initialize the native part of the sample
-        int ret = jniOnCreate(getPackageName());
-        if (0 != ret) {
-            Toast.makeText(this, "jniOnCreate failed with  " + ret, Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-       
+        HandlerThread bus_thread = new HandlerThread("BusHandler");
+        bus_thread.start();
+        bus_handler = new BusHandler(bus_thread.getLooper(), this);
+
+        bus_handler.sendMessage(bus_handler.obtainMessage(BusHandler.ONCREATE, getPackageName()));
     }
 
     @Override
     protected void onDestroy() {
-        jniOnDestroy();
+        //jniOnDestroy();
+        bus_handler.sendEmptyMessage(BusHandler.ONDESTROY);
         super.onDestroy();
     }
-    
+
     private OnClickListener radio_listener = new OnClickListener() {
+        @Override
         public void onClick(View v) {
             // Perform action on clicks
             RadioButton rb = (RadioButton) v;
             if(rb.getId() == R.id.radio_create_session){
-            	Toast.makeText(Chat.this, "You selected " + rb.getText(), Toast.LENGTH_LONG).show();
-            	showDialog(DIALOG_ADVERTISE);
+                Toast.makeText(Chat.this, "You selected " + rb.getText(), Toast.LENGTH_LONG).show();
+                showDialog(DIALOG_ADVERTISE);
             }
             else if (rb.getId() == R.id.radio_join_session){
-            	Toast.makeText(Chat.this, "You selected " + rb.getText(), Toast.LENGTH_LONG).show();
-            	showDialog(DIALOG_JOIN_SESSION);
+                Toast.makeText(Chat.this, "You selected " + rb.getText(), Toast.LENGTH_LONG).show();
+                showDialog(DIALOG_JOIN_SESSION);
             }
-           
-
         }
     };
+
+
     // Send a message to all connected chat clients
     private void chat(String message) {
         // Send the AllJoyn signal to other connected chat clients
-        int ret = sendChatMsg(message);
-        if (0 != ret) {
-            Log.e("AllJoynChat", String.format("sendChatMsg(%s) failed (%d)", message, ret));
-        }
+        bus_handler.sendMessage(bus_handler.obtainMessage(BusHandler.SENDCHATMSG, message));
+        listViewArrayAdapter.add("Me: " + message);
     }
 
     @Override
@@ -161,11 +176,12 @@ public class Chat extends Activity {
 
     public void ChatCallback(String sender, String chatStr)
     {
-        Message msg = handler.obtainMessage(0);
-        msg.obj = String.format("Message from %s: \"%s\"", sender.substring(sender.length() - 10), chatStr);
+        Message msg = handler.obtainMessage(INCOMINGMESSAGE);
+        msg.obj = String.format("%s: \"%s\"", sender.substring(sender.length() - 10), chatStr);
         handler.sendMessage(msg);
     }
 
+    @Override
     protected Dialog onCreateDialog(int id) {
         Dialog dialog;
         switch (id) {
@@ -176,73 +192,79 @@ public class Chat extends Activity {
             dialog.setTitle("Name to be advertised:");
             Button okButton = (Button) dialog.findViewById(R.id.AdvertiseOk);
             okButton.setOnClickListener(new OnClickListener() {
-                                            public void onClick(View v) {
-                                                View r = v.getRootView();
-                                                TextView text = (TextView) r.findViewById(R.id.AdvertiseText);
-                                                boolean isAdvertised = advertise(text.getText().toString());
-                                                Toast.makeText(getApplicationContext(), text.getText().toString(), Toast.LENGTH_LONG).show();
-                                                dismissDialog(DIALOG_ADVERTISE);
-                                                //setContentView(R.layout.main);
+                @Override
+                public void onClick(View v) {
+                    View r = v.getRootView();
+                    TextView text = (TextView) r.findViewById(R.id.AdvertiseText);
+                    bus_handler.sendMessage(bus_handler.obtainMessage(BusHandler.ADVERTISE, text.getText().toString()));
+                    listViewArrayAdapter.add("CHAT SESSION \"" + text.getText().toString() + "\"");
+                    Toast.makeText(getApplicationContext(), text.getText().toString(), Toast.LENGTH_LONG).show();
+                    dismissDialog(DIALOG_ADVERTISE);
+                    //setContentView(R.layout.main);
 
-                                            }
-                                        });
+                }
+            });
             Button cancelButton = (Button) dialog.findViewById(R.id.AdvertiseCancel);
             cancelButton.setOnClickListener(new OnClickListener() {
-                                                public void onClick(View v) {
-                                                    dismissDialog(DIALOG_ADVERTISE);
-                                                    finish();
-                                                }
-                                            });
+                @Override
+                public void onClick(View v) {
+                    dismissDialog(DIALOG_ADVERTISE);
+                    finish();
+                }
+            });
             break;
-        	
+
         case DIALOG_JOIN_SESSION:
-        	dialog = new Dialog(Chat.this);
+            dialog = new Dialog(Chat.this);
             dialog.setContentView(R.layout.joinsession);
             dialog.setTitle("Name of session you want to join:");
             Button joinokButton = (Button) dialog.findViewById(R.id.JoinSessionOk);
             joinokButton.setOnClickListener(new OnClickListener() {
-                                            public void onClick(View v) {
-                                                View r = v.getRootView();
-                                                TextView text = (TextView) r.findViewById(R.id.JoinSessionText);
-                                                boolean isAdvertised = joinSession(text.getText().toString());
-                                                Toast.makeText(getApplicationContext(), text.getText().toString(), Toast.LENGTH_LONG).show();
-                                                dismissDialog(DIALOG_JOIN_SESSION);
-                                                //setContentView(R.layout.main);
-                                            }
-                                        });
+                @Override
+                public void onClick(View v) {
+                    View r = v.getRootView();
+                    TextView text = (TextView) r.findViewById(R.id.JoinSessionText);
+                    bus_handler.sendMessage(bus_handler.obtainMessage(BusHandler.JOINSESSION, text.getText().toString()));
+                    listViewArrayAdapter.add("CHAT SESSION \"" + text.getText().toString() + "\"");
+                    Toast.makeText(getApplicationContext(), text.getText().toString(), Toast.LENGTH_LONG).show();
+                    dismissDialog(DIALOG_JOIN_SESSION);
+                    //setContentView(R.layout.main);
+                }
+            });
             Button joinCancelButton = (Button) dialog.findViewById(R.id.JoinSessionCancel);
             joinCancelButton.setOnClickListener(new OnClickListener() {
-                                                public void onClick(View v) {
-                                                    dismissDialog(DIALOG_JOIN_SESSION);
-                                                    finish();
-                                                }
-                                            });
+                @Override
+                public void onClick(View v) {
+                    dismissDialog(DIALOG_JOIN_SESSION);
+                    finish();
+                }
+            });
             break;
         default:
             dialog = null;
             break;
         }
-        
+
         setContentView(R.layout.main);
-        
+
         listViewArrayAdapter = new ArrayAdapter<String>(this, R.layout.message);
         listView = (ListView) findViewById(R.id.ListView);
         listView.setAdapter(listViewArrayAdapter);
 
         editText = (EditText) findViewById(R.id.EditText);
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-        									public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
-        										if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
-        											String message = view.getText().toString();
-        											Log.d("Chat","\n Calling Chat method ");
-        											chat(message);
-        											view.setText("");
-        										}
-        										return true;
-        									}
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                    String message = view.getText().toString();
+                    chat(message);
+                    view.setText("");
+                }
+                return true;
+            }
         });
 
-       
+
         return dialog;
     }
 
@@ -251,6 +273,65 @@ public class Chat extends Activity {
         if (null != menu) {
             menu.getItem(0).setEnabled(!isConnected);
             menu.getItem(1).setEnabled(isConnected);
+        }
+    }
+
+
+    private static class BusHandler extends Handler {
+        static final int ONCREATE = 1;
+        static final int ONDESTROY = 2;
+        static final int SENDCHATMSG = 3;
+        static final int ADVERTISE = 4;
+        static final int JOINSESSION = 5;
+
+        Chat chat;
+
+        BusHandler(Looper looper, Chat chat) {
+            super(looper);
+            this.chat = chat;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what)
+            {
+            case ONCREATE:
+            {
+                int ret = chat.jniOnCreate((String) msg.obj);
+
+                if (ret != 0) {
+                    Message responseMsg = chat.bus_handler.obtainMessage(SESSIONCREATEFAILED);
+                    responseMsg.arg1 = ret;
+                    chat.handler.sendMessage(responseMsg);
+                }
+
+                break;
+            }
+            case ONDESTROY:
+                chat.jniOnDestroy();
+                break;
+            case SENDCHATMSG:
+            {
+                int ret = chat.sendChatMsg((String) msg.obj);
+                if (ret != 0) {
+                    Message responseMsg = chat.bus_handler.obtainMessage(SENDCHATFAILED);
+                    responseMsg.obj = msg.obj;
+                    responseMsg.arg1 = ret;
+                    chat.handler.sendMessage(responseMsg);
+                }
+
+                break;
+            }
+            case ADVERTISE:
+                // ignore return value
+                chat.advertise((String) msg.obj);
+                break;
+            case JOINSESSION:
+                // ignore return value
+                chat.joinSession((String) msg.obj);
+                break;
+            default: break;
+            }
         }
     }
 
