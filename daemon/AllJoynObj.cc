74a3d952b068900092753b17463a4f0aa465f33d
Steve Kinneberg <stevek@quicinc.com>
2012-07-11 15:13:07 -0700

    ALLJOYN-1220: Fix problem where names found via Bluetooth get flushed when there are no listeners which prevents connecting to those names in the future via Bluetooth.
    Change-Id: I6403f84ebea1caa6952109bbe70f150cf030219c

--- daemon/AllJoynObj.cc
+++ daemon/AllJoynObj.cc
@@ -2426,7 +2426,14 @@ QStatus AllJoynObj::ProcCancelAdvertise(const qcc::String& sender, const qcc::St
         }
 
         if (discoverMap.empty() && advertiseMap.empty()) {
-            nameMap.clear();
+            std::multimap<qcc::String, NameMapEntry>::iterator nmit = nameMap.begin();
+            while (nmit != nameMap.end()) {
+                if ((*nmit).second.transport != TRANSPORT_BLUETOOTH) {
+                    nameMap.erase(nmit++);
+                } else {
+                    ++nmit;
+                }
+            }
         }
 
     } else if (!foundAdvert) {
@@ -2627,7 +2634,14 @@ QStatus AllJoynObj::ProcCancelFindName(const qcc::String& sender, const qcc::Str
         }
 
         if (discoverMap.empty() && advertiseMap.empty()) {
-            nameMap.clear();
+            std::multimap<qcc::String, NameMapEntry>::iterator nmit = nameMap.begin();
+            while (nmit != nameMap.end()) {
+                if ((*nmit).second.transport != TRANSPORT_BLUETOOTH) {
+                    nameMap.erase(nmit++);
+                } else {
+                    ++nmit;
+                }
+            }
         }
 
     } else if (!foundNamePrefix) {
