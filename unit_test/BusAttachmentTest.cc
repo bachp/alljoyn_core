6c328a8ff53897dc77352ebbb3a9f628a234c83b
George Nash <georgen@quicinc.com>
2012-07-11 09:08:06 -0700

    Added unit test BusAttachmentTest.find_multiple_names This test mirrors the JUnit BusAttachmentTest.testFindMultipleNames

--- unit_test/BusAttachmentTest.cc
+++ unit_test/BusAttachmentTest.cc
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include <qcc/String.h>
+#include <qcc/Thread.h>
 
 #include <alljoyn/BusAttachment.h>
 #include <alljoyn/DBusStd.h>
@@ -115,4 +116,99 @@ TEST_F(BusAttachmentTest, FindName_Null_Name)
     /* flag indicates that Fail if name cannot be immediatly obtained */
     status = bus.FindAdvertisedName(requestedName);
     EXPECT_EQ(ER_BAD_ARG_1, status) << "  Actual Status: " << QCC_StatusText(status);
+}
+
+bool foundNameA = false;
+bool foundNameB = false;
+
+class FindMulipleNamesBusListener : public BusListener {
+    void FoundAdvertisedName(const char* name, TransportMask transport, const char* namePrefix) {
+        printf("FoundAdvertisedName name=%s  prefix=%s\n", name, namePrefix);
+        if (strcmp(name, "name.A") == 0) {
+            foundNameA = true;
+        }
+        if (strcmp(name, "name.B") == 0) {
+            foundNameB = true;
+        }
+    }
+};
+
+TEST_F(BusAttachmentTest, find_multiple_names)
+{
+    QStatus status = ER_FAIL;
+    FindMulipleNamesBusListener testBusListener; // = new FindMulipleNamesBusListener();
+    bus.RegisterBusListener(testBusListener);
+
+    foundNameA = false;
+    foundNameB = false;
+
+    status = bus.FindAdvertisedName("name.A");
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = bus.FindAdvertisedName("name.B");
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    BusAttachment otherBus("BusAttachmentTestOther", true);
+    status = otherBus.Start();
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = otherBus.Connect(getConnectArg().c_str());
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    status = otherBus.AdvertiseName("name.A", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = otherBus.AdvertiseName("name.B", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    //Wait upto 8 seconds for the both found name signals to complete.
+    for (int i = 0; i < 800; ++i) {
+        qcc::Sleep(10);
+        if (foundNameA && foundNameB) {
+            break;
+        }
+    }
+
+
+    EXPECT_TRUE(foundNameA);
+    EXPECT_TRUE(foundNameB);
+
+    status = otherBus.CancelAdvertiseName("name.A", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = otherBus.CancelAdvertiseName("name.B", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    status = bus.CancelFindAdvertisedName("name.B");
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    foundNameA = false;
+    foundNameB = false;
+
+    status = otherBus.AdvertiseName("name.A", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = otherBus.AdvertiseName("name.B", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    //Wait upto 2 seconds for the found name signal to complete.
+    for (int i = 0; i < 200; ++i) {
+        qcc::Sleep(10);
+        if (foundNameA) {
+            break;
+        }
+    }
+
+    EXPECT_TRUE(foundNameA);
+    EXPECT_FALSE(foundNameB);
+
+    status = otherBus.CancelAdvertiseName("name.A", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+    status = otherBus.CancelAdvertiseName("name.B", TRANSPORT_ANY);
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    status = bus.CancelFindAdvertisedName("name.A");
+    EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+
+    //Must Unregister bus listener or the test will segfault when trying to call
+    //
+    bus.UnregisterBusListener(testBusListener);
+
+    otherBus.Stop();
+    otherBus.Join();
 }
