61a78f22b7d2c3e7f38f368f559b072ef9d5b50f
Padmapriya Narayanan <padmapri@quicinc.com>
2012-07-14 15:28:58 -0700

    Fix Froyo build issues in ice_client_login_service
    Change-Id: Id60ee79de5fddcc871bdfc15fbf1ab656674003d

--- alljoyn_android/ice_client_login_service/src/org/alljoyn/ice/ClientLoginServiceInterface.java
+++ alljoyn_android/ice_client_login_service/src/org/alljoyn/ice/ClientLoginServiceInterface.java
@@ -17,13 +17,8 @@
 package org.alljoyn.ice;
 
 
-import java.util.Map;
-
-import org.alljoyn.bus.BusException;
 import org.alljoyn.bus.annotation.BusInterface;
 import org.alljoyn.bus.annotation.BusMethod;
-import org.alljoyn.bus.annotation.Position;
-import org.alljoyn.bus.annotation.Signature;
 
 @BusInterface(name = "org.alljoyn.ice.clientloginservice")
 public interface ClientLoginServiceInterface {
