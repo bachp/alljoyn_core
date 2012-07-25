5c8fd529497962e74949bd3488fdcf5854422c2f
Sheshambika Venkateshwaran <sheshamb@quicinc.com>
2012-07-25 11:50:41 -0700

    ALLJOYN-1255: Ensure that the DiscoveryManager thread has exited before deleting the BusAttachment. Make sure that the pktStream is valid in DaemonICETransport::Connect.

--- daemon/ice/DaemonICETransport.cc
+++ daemon/ice/DaemonICETransport.cc
@@ -855,6 +855,11 @@ QStatus DaemonICETransport::Join(void)
     /* Join the timer */
     daemonICETransportTimer.Join();
 
+    /* Join the discovery manager */
+    if (m_dm) {
+        m_dm->Join();
+    }
+
     m_stopping = false;
 
     return ER_OK;
@@ -1962,9 +1967,14 @@ QStatus DaemonICETransport::Connect(const char* connectSpec, const SessionOpts& 
         }
     }
 
+    /* Make sure we have a pktStream */
+    if (!pktStream || !pktStream->HasSocket()) {
+        status = ER_BUS_CONNECT_FAILED;
+    }
+
     /* If we created or reused an ICEPacketStream, the wrap it in a DamonICEEndpoint */
     DaemonICEEndpoint* conn = NULL;
-    if ((status == ER_OK) && pktStream) {
+    if (status == ER_OK) {
         conn = new DaemonICEEndpoint(this, m_bus, false, normSpec, *pktStream);
         /* Setup the PacketEngine connection */
         status = conn->PacketEngineConnect(pktStream->GetICERemoteAddr(), pktStream->GetICERemotePort());
