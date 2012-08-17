09c174da4d76cdc6f009017e83be75b4ff1142a1
Padmapriya Narayanan <padmapri@quicinc.com>
2012-08-17 15:28:09 -0700

    Change the value of STUN_OVERHEAD in daemon/winrt/ICEPacketStream.h to conform to that in daemon/posix/ICEPacketStream.h
    Change-Id: I712aacc175edc340a354eb48decca5b7f7dd778e

--- daemon/winrt/ICEPacketStream.h
+++ daemon/winrt/ICEPacketStream.h
@@ -41,9 +41,13 @@ namespace ajn {
 class ICEPacketStream : public PacketStream {
   public:
 
+    /* The value of TURN_ACCT_TOKEN_MAX_SIZE + TURN_ACCT_TOKEN_ATTRIBUTE_HEADER_SIZE accounts for the max size of the
+     * TURN username attribute. As this is something that is sent by the server during run time, there is no way
+     * for us to know that statically during initialization. */
     static const uint32_t STUN_OVERHEAD = StunMessage::HEADER_SIZE + StunAttributeXorPeerAddress::ATTR_SIZE_WITH_HEADER +
                                           StunAttribute::ATTR_HEADER_SIZE + StunAttributeMessageIntegrity::ATTR_SIZE_WITH_HEADER +
-                                          StunAttributeFingerprint::ATTR_SIZE_WITH_HEADER;
+                                          StunAttributeFingerprint::ATTR_SIZE_WITH_HEADER + TURN_ACCT_TOKEN_MAX_SIZE +
+                                          TURN_ACCT_TOKEN_ATTRIBUTE_HEADER_SIZE;
 
     /** Construct a PacketDest from a addr,port */
     static PacketDest GetPacketDest(const qcc::IPAddress& addr, uint16_t port);
