6e4588afc58de2931e74e76fb60babcb514997d9
Gregory Arias <c_garias@quicinc.com>
2012-08-17 10:29:32 -0700

    Enabled ProximityScanner under iOS/OSX
    Change-Id: Id416a5a6bfcdc99c44baaa5697b697067078825a

--- daemon/ice/DiscoveryManager.h
+++ daemon/ice/DiscoveryManager.h
@@ -52,7 +52,7 @@
 
 using namespace qcc;
 
-#if defined(QCC_OS_ANDROID) || defined(QCC_OS_WINRT)
+#if defined(QCC_OS_ANDROID) || defined(QCC_OS_DARWIN) || defined(QCC_OS_WINRT)
 #define ENABLE_PROXIMITY_FRAMEWORK
 #endif
 
