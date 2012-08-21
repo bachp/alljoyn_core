8591534271fb6c2a382eb8ad58312102a98bc5cb
Mat Martineau <mathewm@quicinc.com>
2012-08-21 14:50:38 -0700

    P2P: Remove UI from service
    Change-Id: Ia32183d57de496f8b70928a20b5c8455a57ff75a

--- alljoyn_android/alljoyn_android_p2pservice/src/org/alljoyn/bus/p2p/service/P2pHelperAndroidService.java
+++ alljoyn_android/alljoyn_android_p2pservice/src/org/alljoyn/bus/p2p/service/P2pHelperAndroidService.java
@@ -18,14 +18,8 @@ package org.alljoyn.bus.p2p.service;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.os.Binder;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
 
 import android.app.Service;
-import android.app.Notification;
-import android.app.PendingIntent;
 
 import android.content.Intent;
 import android.content.Context;
@@ -49,30 +43,11 @@ public class P2pHelperAndroidService extends Service {
         return mBinder;
     }
 
-    private static final int NOTIFICATION_ID = 0xca11ab1e;
-
-    private Object mBus = null; // TODO: replace with JNI handle
     P2pHelperService mP2pHelperService = null;
-    BackgroundHandler mBackgroundHandler = null;
     private P2pManager mP2pManager = null;
 
     public void onCreate() {
         Log.i(TAG, "onCreate()");
-
-        CharSequence title = "AllJoyn";
-        CharSequence message = "AllJoyn Wi-Fi Direct Helper Service";
-        Intent intent = new Intent(this, P2pHelperAndroidActivity.class );
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        Notification notification = new Notification(R.drawable.p2p_icon, null, System.currentTimeMillis());
-        notification.setLatestEventInfo(this, title, message, pendingIntent);
-        notification.flags |= Notification.DEFAULT_SOUND | Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR;
-
-        Log.i(TAG, "onCreate(): startForeground()");
-        startForeground(NOTIFICATION_ID, notification);
-
-        HandlerThread handlerThread = new HandlerThread("BackgroundHandler");
-        handlerThread.start();
-        mBackgroundHandler = new BackgroundHandler(handlerThread.getLooper());
 
         DoStartTask doStartTask = new DoStartTask();
         doStartTask.execute(this);
@@ -89,8 +64,6 @@ public class P2pHelperAndroidService extends Service {
         Log.i(TAG, "onStartCommand()");
         return START_STICKY;
     }
-
-    private static final String SERVICE_NAME = "org.alljoyn.bus.p2p";
 
     private class DoStartTask extends AsyncTask<Context, Integer, String> {
         @Override
@@ -229,163 +202,5 @@ public class P2pHelperAndroidService extends Service {
     public void exit() {
         Log.i(TAG, "exit");
         System.exit(0);
-    }
-
-    public void onFoundAdvertisedName(String name, String namePrefix, String guid, String device) {
-        Log.i(TAG, "onFoundAdvertisedName("
-              + name + ", "
-              + namePrefix + ", "
-              + guid + ", "
-              + device + "): Sending to BackgroundHandler");
-        mBackgroundHandler.onFoundAdvertisedName(name, namePrefix, guid, device);
-    }
-
-    public void onLostAdvertisedName(String name, String namePrefix, String guid, String device) {
-        Log.i(TAG, "onLostAdvertisedName("
-              + name + ", "
-              + namePrefix + ", "
-              + guid + ", "
-              + device + "): Sending to BackgroundHandler");
-        mBackgroundHandler.onLostAdvertisedName(name, namePrefix, guid, device);
-    }
-
-    public void onLinkEstablished(int handle) {
-        Log.i(TAG, "onLinkEstablished(" + handle + "): Sending to BackgroundHandler");
-        mBackgroundHandler.onLinkEstablished(handle);
-    }
-
-    public void onLinkError(int handle, int error) {
-        Log.i(TAG, "onLinkError(" + handle + ", " + error + "): Sending to BackgroundHandler");
-        mBackgroundHandler.onLinkError(handle, error);
-    }
-
-    public void onLinkLost(int handle) {
-        Log.i(TAG, "onLinkLost(" + handle + "): Sending to BackgroundHandler");
-        mBackgroundHandler.onLinkLost(handle);
-    }
-
-    private class MessageArgs {
-        public MessageArgs(String name, String namePrefix, String guid, String device) {
-            mName = name;
-            mNamePrefix = namePrefix;
-            mGuid = guid;
-            mDevice = device;
-        }
-
-        String mName;
-        String mNamePrefix;
-        String mGuid;
-        String mDevice;
-    }
-
-    private final class BackgroundHandler extends Handler {
-        public BackgroundHandler(Looper looper) {
-            super(looper);
-        }
-
-        public void onFoundAdvertisedName(String name, String namePrefix, String guid, String device) {
-            Log.i(TAG, "BackgroundHandler.onFoundAdvertisedName("
-                  + name + ", "
-                  + namePrefix + ", "
-                  + guid + ", "
-                  + device + "): Queueing message");
-            Message msg = mBackgroundHandler.obtainMessage(DO_ON_FOUND_ADVERTISED_NAME,
-                                                           new MessageArgs(name, namePrefix, guid, device));
-            mBackgroundHandler.sendMessage(msg);
-        }
-
-        public void onLostAdvertisedName(String name, String namePrefix, String guid, String device) {
-            Log.i(TAG, "BackgroundHandler.onLostAdvertisedName("
-                  + name + ", "
-                  + namePrefix + ", "
-                  + guid + ", "
-                  + device + "): Queueing message");
-            Message msg = mBackgroundHandler.obtainMessage(DO_ON_LOST_ADVERTISED_NAME,
-                                                           new MessageArgs(name, namePrefix, guid, device));
-            mBackgroundHandler.sendMessage(msg);
-        }
-
-        public void onLinkEstablished(int handle) {
-            Log.i(TAG, "BackgroundHandler.onLinkEstablished(" + handle + "): Queueing message");
-            Message msg = mBackgroundHandler.obtainMessage(DO_ON_LINK_ESTABLISHED, handle, 0, null);
-            mBackgroundHandler.sendMessage(msg);
-        }
-
-        public void onLinkError(int handle, int error) {
-            Log.i(TAG, "BackgroundHandler.onLinkError(" + handle + ", " + error + "): Queueing message");
-            Message msg = mBackgroundHandler.obtainMessage(DO_ON_LINK_ERROR, handle, error, null);
-            mBackgroundHandler.sendMessage(msg);
-        }
-
-        public void onLinkLost(int handle) {
-            Log.i(TAG, "BackgroundHandler.onLinkLost(" + handle + "): Queueing message");
-            Message msg = mBackgroundHandler.obtainMessage(DO_ON_LINK_LOST, handle, 0, null);
-            mBackgroundHandler.sendMessage(msg);
-        }
-
-        private static final int DO_ON_FOUND_ADVERTISED_NAME = 1;
-        private static final int DO_ON_LOST_ADVERTISED_NAME = 2;
-        private static final int DO_ON_LINK_ESTABLISHED = 3;
-        private static final int DO_ON_LINK_ERROR = 4;
-        private static final int DO_ON_LINK_LOST = 5;
-
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-            case DO_ON_FOUND_ADVERTISED_NAME:
-                doOnFoundAdvertisedName(((MessageArgs)msg.obj).mName, ((MessageArgs)msg.obj).mNamePrefix,
-                                        ((MessageArgs)msg.obj).mGuid, ((MessageArgs)msg.obj).mDevice);
-                break;
-
-            case DO_ON_LOST_ADVERTISED_NAME:
-                doOnLostAdvertisedName(((MessageArgs)msg.obj).mName, ((MessageArgs)msg.obj).mNamePrefix,
-                                       ((MessageArgs)msg.obj).mGuid, ((MessageArgs)msg.obj).mDevice);
-                break;
-
-            case DO_ON_LINK_ESTABLISHED:
-                doOnLinkEstablished(msg.arg1);
-                break;
-
-            case DO_ON_LINK_ERROR:
-                doOnLinkError(msg.arg1, msg.arg2);
-                break;
-
-            case DO_ON_LINK_LOST:
-                doOnLinkLost(msg.arg1);
-                break;
-            }
-        }
-
-        public void doOnFoundAdvertisedName(String name, String namePrefix, String guid, String device) {
-            Log.i(TAG, "BackgroundHandler.doOnFoundAdvertisedName("
-                  + name + ", "
-                  + namePrefix + ", "
-                  + guid + ", "
-                  + device + "): Sending signal");
-            mP2pHelperService.OnFoundAdvertisedName(name, namePrefix, guid, device);
-        }
-
-        public void doOnLostAdvertisedName(String name, String namePrefix, String guid, String device) {
-            Log.i(TAG, "BackgroundHandler.doOnLostAdvertisedName("
-                  + name + ", "
-                  + namePrefix + ", "
-                  + guid + ", "
-                  + device + "): Sending signal");
-            mP2pHelperService.OnLostAdvertisedName(name, namePrefix, guid, device);
-        }
-
-        public void doOnLinkEstablished(int handle) {
-            Log.i(TAG, "BackgroundHandler.doOnLinkEstablished(" + handle + "): Sending signal");
-            mP2pHelperService.OnLinkEstablished(handle);
-        }
-
-        public void doOnLinkError(int handle, int error) {
-            Log.i(TAG, "BackgroundHandler.doOnLinkError(" + handle + ", " + error + "): Sending signal");
-            mP2pHelperService.OnLinkError(handle, error);
-        }
-
-        public void doOnLinkLost(int handle) {
-            Log.i(TAG, "BackgroundHandler.doOnLinkLost(" + handle + "): Sending signal");
-            mP2pHelperService.OnLinkLost(handle);
-        }
     }
 }
