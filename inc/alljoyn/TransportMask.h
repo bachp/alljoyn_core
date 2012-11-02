feb3a8996467afa08063df0d716a6b92f1e4f128
Craig Dowell <cdowell@quicinc.com>
2012-11-02 14:49:53 -0700

    Turn off Wi-Fi Direct by default
    Change-Id: If95c026529271c0201f299a0400f0382733a11f2

--- inc/alljoyn/TransportMask.h
+++ inc/alljoyn/TransportMask.h
@@ -29,7 +29,7 @@ namespace ajn {
 typedef uint16_t TransportMask;
 
 const TransportMask TRANSPORT_NONE      = 0x0000;   /**< no transports */
-const TransportMask TRANSPORT_ANY       = 0xFFFF;   /**< ANY transport */
+
 const TransportMask TRANSPORT_LOCAL     = 0x0001;   /**< Local (same device) transport */
 const TransportMask TRANSPORT_BLUETOOTH = 0x0002;   /**< Bluetooth transport */
 const TransportMask TRANSPORT_TCP       = 0x0004;   /**< Transport using TCP (same as TRANSPORT_WLAN) */
@@ -39,6 +39,25 @@ const TransportMask TRANSPORT_LAN       = 0x0010;   /**< Wired local-area networ
 const TransportMask TRANSPORT_ICE       = 0x0020;   /**< Transport using ICE protocol */
 const TransportMask TRANSPORT_PROXIMITY = 0x0040;   /**< Transport using WinRT Proximity Framework */
 const TransportMask TRANSPORT_WFD       = 0x0080;   /**< Transport using Wi-Fi Direct transport */
+
+/**
+ * A constant indicating that any transport is acceptable.
+ *
+ * It is the case that (1) certain topologies of AllJoyn distributed
+ * applications can cause problems when run on Wi-Fi Direct substrate networks;
+ * (2) the specifics of authentication in Wi-Fi Direct networks can also produce
+ * surprising results in some AllJoyn topologies; and (3) there are
+ * implementation problems in existing Wi-Fi Direct systems that prevent certain
+ * AllJoyn topologies from forming.  Because these issues might produce
+ * surprising results in existing applications that are unaware of the
+ * limitations, we do no enable Wi-Fi Direct automatically.
+ *
+ * Selecting ANY transport really means selecting ANY but Wi-Fi Direct.  In
+ * order to enable discovery over Wi-Fi Direct, and use of the Wi-Fi Direct
+ * transport an application author must positively act and OR in the
+ * TRANSPORT_WFD bit.
+ */
+const TransportMask TRANSPORT_ANY       = (0xFFFF & ~TRANSPORT_WFD);
 }
 
 #endif
